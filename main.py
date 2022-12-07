ffcd892a557e236544ac53a5618aa0d715c23860
Om Gandhi <omgandhi255@gmail.com>
2022-12-07 16:59:53 +0530

    Image Generation code inserted

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 # openai.Model.list()
 # json_string=json.dumps(openai.Model.retrieve("text-davinci-003").__dict__)
 # model=json.loads(json_string)
@@ -10,7 +10,7 @@ openai.api_key = "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"
 # print(model["_response_ms"])
 prompt = openai.Completion.create(
     model="text-davinci-003",
-    prompt="Difference between Parallel and Distributed Systems",
+    prompt="Write SQL code to Select All data from Students Table ",
     max_tokens=1000,
     temperature=0
 )
@@ -20,3 +20,13 @@ model = json.loads(json_string)
 # print(type(model))
 # print(model['_previous']['choices'][0])
 print(model['_previous']['choices'][0]['text'])
+image=openai.Image.create(
+    prompt="Doctor operating on a Sea otter",
+    n=1,
+    size="1024x1024",
+)
+# print(image)
+json_string1=json.dumps(image.__dict__)
+image_model=json.loads(json_string1)
+# print(image_model)
+print(image_model['_previous']['data'][0]['url'])
